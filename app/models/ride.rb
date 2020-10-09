12c545347c50acce09d69dc3f1b4d10c461d9e2f
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-09 15:53:27 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,25 @@
 class Ride < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :attraction
+
+    def take_ride
+        case
+            when user.height < attraction.min_height && user.tickets < attraction.tickets
+                "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}." 
+            when user.height < attraction.min_height
+                "Sorry. You are not tall enough to ride the #{attraction.name}." 
+            when user.tickets < attraction.tickets
+                "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+            else
+                user.update(
+                    tickets: user.tickets - attraction.tickets,
+                    nausea: user.nausea + attraction.nausea_rating,
+                    happiness: user.happiness + attraction.happiness_rating
+                )
+                
+        end      
+    end
+
+    
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ActiveRecord::Base
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    has_secure_password
+    
+    validates :name, presence: true, uniqueness: true
+    
+    def not_admin?
+        !self.admin
+    end 
+
+    def mood
+        if not_admin?
+            if self.nausea > self.happiness
+                @mood = "sad"
+            elsif self.happiness > self.nausea
+                @mood = "happy"
+            end
+        end
+    end
+
 end
